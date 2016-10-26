e3e6c36b3bc252458391a1b15cdb1dcb3e21ca31
Mane, Ajinkya Uday <ajinkya.mane@ge.com>
2016-10-26 21:51:06 +0530

    checkstyle_reduce_warnings

--- Fibonacci/src/FibonacciUI.java
+++ Fibonacci/src/FibonacciUI.java
@@ -10,40 +10,32 @@ public class FibonacciUI
 * @param args
 * Command-line arguments (not used)
 */
-public static void main(String[] args)
-{
+public static void main(String[] args){
  long input = 0, result;
  boolean keepRunning = true;
  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- while (keepRunning == true) 
- {
-  System.out.println("Enter Integer to Calculate Fibonacci value, or q to quit:");
-  String inputString;
-  try
-  {
+ while (keepRunning == true) {
+	System.out.println("Enter Integer to Calculate Fibonacci value, or q to quit:");
+	String inputString;
+  try{
 	inputString = br.readLine();
   }
-  catch(IOException e)
-  {
-	System.err.println("Read failed.");
-	keepRunning = false;
-	continue;
+  catch(IOException e){
+		System.err.println("Read failed.");
+		keepRunning = false;
+		continue;
   }
-  if (inputString.contains("q")) 
-  {
-	keepRunning = false;
-	continue;
+  if (inputString.contains("q")) {
+		keepRunning = false;
+		continue;
   }
-  else
-  {
-   try
-   {
-    input = Long.parseLong(inputString);
+  else{
+   try{
+		input = Long.parseLong(inputString);
    }
-   catch(NumberFormatException nfe )
-   {
-    System.err.println("Invalid Format! Did you enter a integer?");
-	continue;
+   catch(NumberFormatException nfe ){
+		System.err.println("Invalid Format! Did you enter a integer?");
+		continue;
    }
    result = Fibonacci.calculate(input);
    System.out.println("Fibonacci result is: " + result);
@@ -51,5 +43,4 @@ public static void main(String[] args)
  }
  System.out.println("Exiting");
 }
-
 }
